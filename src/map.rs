ff532c6c13399b4d7138be75049b8e587e67ded1
Gen Ichino <zenoplex@gmail.com>
2021-05-23 01:22:36 +0900

    Initial commit

---
+++ src/map.rs
@@ -0,0 +1,39 @@
+use bracket_lib::prelude::to_cp437;
+
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Map {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Floor => ctx.set(x, y, YELLOW, BLACK, to_cp437('.')),
+                    TileType::Wall => ctx.set(x, y, GREEN, BLACK, to_cp437('#')),
+                }
+            }
+        }
+    }
+}
+
+pub fn map_idx(x: u32, y: u32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
