387616a525bba02bde72779c26c5f02db2d30474
Gen Ichino <zenoplex@gmail.com>
2021-05-24 23:24:30 +0900

    feat: use external font

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: u32 = 80;
     pub const SCREEN_HEIGHT: u32 = 50;
+    pub const DISPLAY_WIDTH: u32 = SCREEN_WIDTH;
+    pub const DISPLAY_HEIGHT: u32 = SCREEN_HEIGHT;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
@@ -39,9 +41,15 @@ impl State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())
