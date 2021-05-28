69ade2303bcc4a7b9139463cd0417a231a98ebe2
Gen Ichino <zenoplex@gmail.com>
2021-05-29 00:35:37 +0900

    fix: modify to go down

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -11,7 +11,7 @@ pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
             0 => Point::new(-1, 0),
             1 => Point::new(1, 0),
             2 => Point::new(0, -1),
-            _ => Point::new(0, -1),
+            _ => Point::new(0, 1),
         } + *pos;
 
         if map.can_enter_tile(destination) {
