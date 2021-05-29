338fc3fd7826b35a291c0a500bf97a70ddf87551
Gen Ichino <zenoplex@gmail.com>
2021-05-30 00:40:54 +0900

    feat: add player combat system

--- src/systems.rs
+++ src/systems.rs
@@ -1,3 +1,4 @@
+mod combat;
 mod end_turn;
 mod entity_render;
 mod hud;
@@ -22,6 +23,8 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(combat::combat_system())
+        .flush()
         .add_system(movements::movements_system())
         .flush()
         .add_system(map_render::map_render_system())
