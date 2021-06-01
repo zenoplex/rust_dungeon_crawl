c691c7f549f8c55be066d6e1265684f4a56e4a15
Gen Ichino <zenoplex@gmail.com>
2021-06-02 00:30:07 +0900

    feat: render map based on fov

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -1,16 +1,21 @@
 use crate::prelude::*;
 
 #[system]
-pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera) {
+#[read_component(FieldOfView)]
+#[read_component(Player)]
+pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Camera) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
+
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
+    let player_fov = fov.iter(ecs).next().unwrap();
 
     for y in camera.top_y..=camera.bottom_y {
         for x in camera.left_x..camera.right_x {
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
 
-            if map.is_in_bounds(pt) {
+            if map.is_in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
                 let idx = map_idx(pt.x, pt.y);
                 let glpth = match map.tiles[idx] {
                     TileType::Floor => to_cp437('.'),
