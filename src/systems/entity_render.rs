2e232621a59cdc7e51f2852180906b5cf0d6edc5
Gen Ichino <zenoplex@gmail.com>
2021-06-02 22:40:45 +0900

    feat: render entities only if tile is visible

--- src/systems/entity_render.rs
+++ src/systems/entity_render.rs
@@ -3,14 +3,22 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Render)]
+#[read_component(Player)]
+#[read_component(FieldOfView)]
 pub fn entity_render(ecs: &mut SubWorld, #[resource] camera: &Camera) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(1);
     let offset = Point::new(camera.left_x, camera.top_y);
 
     let mut entities = <(&Point, &Render)>::query();
+
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
+    let player_fov = fov.iter(ecs).next().unwrap();
+
     entities.iter(ecs).for_each(|(pos, render)| {
-        draw_batch.set(*pos - offset, render.color, render.glyph);
+        if player_fov.visible_tiles.contains(pos) {
+            draw_batch.set(*pos - offset, render.color, render.glyph);
+        }
     });
 
     draw_batch.submit(5000).expect("Batch error");
