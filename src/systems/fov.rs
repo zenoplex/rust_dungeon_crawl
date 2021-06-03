206acb82e91f3610ea4b0e52f4f78de3b6d505d0
Gen Ichino <zenoplex@gmail.com>
2021-06-03 21:17:30 +0900

    feat: update visible tiles on move

--- src/systems/fov.rs
+++ src/systems/fov.rs
@@ -7,6 +7,7 @@ pub fn fov(ecs: &mut SubWorld, #[resource] map: &Map) {
     let mut views = <(&Point, &mut FieldOfView)>::query();
     views
         .iter_mut(ecs)
+        // fov.is_dirty is set true in movements system only when entity can move
         .filter(|(_, fov)| fov.is_dirty)
         .for_each(|(pos, mut fov)| {
             fov.visible_tiles = field_of_view_set(*pos, fov.radius, map);
