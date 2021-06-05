1ab5cb5be24cfba52b20a7c47defbf1da712dc39
Gen Ichino <zenoplex@gmail.com>
2021-06-05 10:03:55 +0900

    feat: render spatial map

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -15,14 +15,17 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
 
-            if map.is_in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
-                let idx = map_idx(pt.x, pt.y);
+            let idx = map_idx(pt.x, pt.y);
+            let is_revlead = map.revealed_tiles[idx];
+            let is_visible = player_fov.visible_tiles.contains(&pt);
+            if map.is_in_bounds(pt) && (is_visible | is_revlead) {
+                let tint = if is_visible { WHITE } else { DARK_GRAY };
                 let glpth = match map.tiles[idx] {
                     TileType::Floor => to_cp437('.'),
                     TileType::Wall => to_cp437('#'),
                 };
 
-                draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), glpth);
+                draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), glpth);
             }
         }
     }
