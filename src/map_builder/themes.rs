4360932549ffbbaed1420569e5600330f53d3b1f
Gen Ichino <zenoplex@gmail.com>
2021-06-10 23:20:12 +0900

    feat: add DungeonTheme struct

---
+++ src/map_builder/themes.rs
@@ -0,0 +1,19 @@
+use super::MapTheme;
+use crate::prelude::*;
+
+pub struct DungeonTheme {}
+
+impl DungeonTheme {
+    pub fn new() -> Box<dyn MapTheme> {
+        Box::new(Self {})
+    }
+}
+
+impl MapTheme for DungeonTheme {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
+        match tile_type {
+            TileType::Floor => to_cp437('.'),
+            TileType::Wall => to_cp437('.'),
+        }
+    }
+}
