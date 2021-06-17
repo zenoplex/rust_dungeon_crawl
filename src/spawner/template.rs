6c205afe9f1a7fadc6e0b637dbc62982e0752526
Gen Ichino <zenoplex@gmail.com>
2021-06-17 23:35:14 +0900

    feat: load external file

---
+++ src/spawner/template.rs
@@ -0,0 +1,35 @@
+use crate::prelude::*;
+use legion::systems::CommandBuffer;
+use ron::de::from_reader;
+use serde::Deserialize;
+use std::collections::HashSet;
+use std::fs::File;
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Template {
+    pub entity_type: EntityType,
+    pub levels: HashSet<usize>,
+    pub frequency: i32,
+    pub name: String,
+    pub glyph: char,
+    pub provies: Option<Vec<(String, i32)>>,
+    pub hp: Option<i32>,
+}
+
+#[derive(Clone, Deserialize, Debug, PartialEq)]
+pub enum EntityType {
+    Enemy,
+    Item,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Templates {
+    pub entities: Vec<Template>,
+}
+
+impl Templates {
+    pub fn load() -> Self {
+        let file = File::open("resources/template.ron").expect("Failed to open file");
+        from_reader(file).expect("Unable to load templates")
+    }
+}
