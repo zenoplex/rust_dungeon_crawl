ac86e109b19a259f3397bbe2968b8dcfd158cd9d
Gen Ichino <zenoplex@gmail.com>
2021-06-20 00:41:46 +0900

    feat: apply weapon damage

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -4,28 +4,50 @@ use crate::prelude::*;
 #[read_component(WantsToAttack)]
 #[read_component(Player)]
 #[write_component(Health)]
+#[read_component(Damage)]
+#[read_component(Carried)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     // Entity is required since we need to remove message
     let mut attackers = <(Entity, &WantsToAttack)>::query();
-    let victims: Vec<(Entity, Entity)> = attackers
+    // Entities of Vec<WantToAttack, Attacker, Victim>
+    let victims: Vec<(Entity, Entity, Entity)> = attackers
         .iter(ecs)
-        .map(|(message, wants_to_attack)| (*message, wants_to_attack.victim))
+        .map(|(message, wants_to_attack)| {
+            (*message, wants_to_attack.attacker, wants_to_attack.victim)
+        })
         .collect();
 
-    victims.iter().for_each(|(message, victim)| {
+    victims.iter().for_each(|(message, attacker, victim)| {
         let is_player = ecs
             .entry_ref(*victim)
             .unwrap()
             .get_component::<Player>()
             .is_ok();
 
+        let base_damage = if let Ok(attacker_entry) = &ecs.entry_ref(*attacker) {
+            if let Ok(dmg) = attacker_entry.get_component::<Damage>() {
+                dmg.0
+            } else {
+                0
+            }
+        } else {
+            0
+        };
+
+        let weapon_damage: i32 = <(&Carried, &Damage)>::query()
+            .iter(ecs)
+            .filter(|(carried, _)| carried.0 == *attacker)
+            .map(|(_, dmg)| dmg.0)
+            .sum();
+
+        let damage = base_damage + weapon_damage;
+
         if let Ok(mut health) = ecs
             .entry_mut(*victim)
             .unwrap()
             .get_component_mut::<Health>()
         {
-            println!("Health before attack: {}", health.current);
-            health.current -= 1;
+            health.current -= damage;
             // Do not remove Player entity
             if health.current < 1 && !is_player {
                 commands.remove(*victim);
